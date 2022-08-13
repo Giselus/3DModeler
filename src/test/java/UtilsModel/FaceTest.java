cc4f35b0b90f7347b7dcb5a18ffe9ff10deee5ae
julianles <julian.lesniak@student.uj.edu.pl>
2022-08-13 14:12:31 +0200

    Test to Face

--- src/test/java/UtilsModel/FaceTest.java
+++ src/test/java/UtilsModel/FaceTest.java
@@ -4,7 +4,9 @@ import org.joml.Vector3f;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FaceTest {
@@ -14,6 +16,16 @@ class FaceTest {
         VertexPosition v2 = new VertexPosition(new Vector3f(1f, 0f, 0f));
         VertexPosition v3 = new VertexPosition(new Vector3f(0f, 0f, 1f));
 
-        //TODO weird dependencies
+        ArrayList<VertexPosition> vertices = new ArrayList<>();
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        Face face = new Face(vertices);
+
+        ArrayList<VertexInstance> result = face.getVertices();
+        List<VertexPosition> resultPositions = result.stream().map(VertexInstance::getPosition).toList();
+
+        assertThat(resultPositions).containsExactly(v1, v2, v3);
     }
 }
