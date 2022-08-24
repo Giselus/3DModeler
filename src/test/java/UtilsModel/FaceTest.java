855ae974d7d09ada03c93213816c52f39eb01fb7
julianles <julian.lesniak@student.uj.edu.pl>
2022-08-24 13:50:36 +0200

    Tests for Face

--- src/test/java/UtilsModel/FaceTest.java
+++ src/test/java/UtilsModel/FaceTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FaceTest {
     @Test
@@ -23,9 +21,81 @@ class FaceTest {
 
         Face face = new Face(vertices);
 
+        Vector3f expectedNormal = new Vector3f(-.577f, -.577f, -.577f);
+
         ArrayList<VertexInstance> result = face.getVertices();
         List<VertexPosition> resultPositions = result.stream().map(VertexInstance::getPosition).toList();
+        Vector3f resultNormal = face.getNormal();
 
         assertThat(resultPositions).containsExactly(v1, v2, v3);
+        assertThat(resultNormal.x).isEqualTo(expectedNormal.x, withPrecision(.001f));
+        assertThat(resultNormal.y).isEqualTo(expectedNormal.y, withPrecision(.001f));
+        assertThat(resultNormal.z).isEqualTo(expectedNormal.z, withPrecision(.001f));
+    }
+
+    @Test
+    public void testIsPicked() {
+        VertexPosition v1 = new VertexPosition(new Vector3f(0f, 1f, 0f));
+        VertexPosition v2 = new VertexPosition(new Vector3f(1f, 0f, 0f));
+        VertexPosition v3 = new VertexPosition(new Vector3f(0f, 0f, 1f));
+
+        ArrayList<VertexPosition> vertices = new ArrayList<>();
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        Face face = new Face(vertices);
+
+        boolean expected = false;
+
+        boolean result = face.isPicked();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void testPick() {
+        VertexPosition v1 = new VertexPosition(new Vector3f(0f, 1f, 0f));
+        VertexPosition v2 = new VertexPosition(new Vector3f(1f, 0f, 0f));
+        VertexPosition v3 = new VertexPosition(new Vector3f(0f, 0f, 1f));
+
+        ArrayList<VertexPosition> vertices = new ArrayList<>();
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        Face face = new Face(vertices);
+
+        face.pick();
+
+        boolean expected = true;
+
+        boolean result = face.isPicked();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void testUnPick() {
+        VertexPosition v1 = new VertexPosition(new Vector3f(0f, 1f, 0f));
+        VertexPosition v2 = new VertexPosition(new Vector3f(1f, 0f, 0f));
+        VertexPosition v3 = new VertexPosition(new Vector3f(0f, 0f, 1f));
+
+        ArrayList<VertexPosition> vertices = new ArrayList<>();
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        Face face = new Face(vertices);
+
+        face.pick();
+
+        face.unpick();
+
+        boolean expected = false;
+
+        boolean result = face.isPicked();
+
+        assertThat(result).isEqualTo(expected);
     }
 }
