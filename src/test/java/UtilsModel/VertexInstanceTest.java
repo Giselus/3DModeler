7acaa286e51ee422521a89f187463d40d232a0c2
julianles <julian.lesniak@student.uj.edu.pl>
2022-08-24 13:52:28 +0200

    Tests for VertexInstance

--- src/test/java/UtilsModel/VertexInstanceTest.java
+++ src/test/java/UtilsModel/VertexInstanceTest.java
@@ -1,12 +1,13 @@
 package UtilsModel;
 
-import UtilsCommon.NormalVector;
+//import UtilsCommon.NormalVector;
 import org.joml.Vector3f;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VertexInstanceTest {
@@ -14,27 +15,25 @@ class VertexInstanceTest {
     VertexPosition v1;
     VertexPosition v2;
     VertexPosition v3;
-    NormalVector normal;
     Face face;
     void setUp(){
         v1 = new VertexPosition(new Vector3f(0f, 1f, 0f));
         v2 = new VertexPosition(new Vector3f(0f, 3f, 1f));
         v3 = new VertexPosition(new Vector3f(0f, 2f, 8f));
 
-        normal = new NormalVector(new Vector3f(1f, 0f, 0f));
         face = new Face(new ArrayList<>(List.of(new VertexPosition[]{v1, v2, v3})));
     }
     @Test
     public void testConstructor(){
         setUp();
-        VertexPosition result = new VertexInstance(v1, face, normal).getPosition();
+        VertexPosition result = new VertexInstance(v1, face).getPosition();
         assertEquals(v1, result);
     }
     @Test
     public void testGetBufferedData(){
         setUp();
         float[] expected = {0f, 1f, 0f, 1f, 0f, 0f, -1f};
-        var result = new VertexInstance(v1, face, normal).getBufferedData();
+        var result = new VertexInstance(v1, face).getBufferedData();
         for(int i=0; i< expected.length; i++){
             assertEquals(expected[i], result[i], 0.001f);
         }
@@ -45,21 +44,22 @@ class VertexInstanceTest {
         setUp();
         v1.pick();
         float[] expected = {0f, 1f, 0f, 1f, 0f, 0f, 1f};
-        var result = new VertexInstance(v1, face, normal).getBufferedData();
+        var result = new VertexInstance(v1, face).getBufferedData();
         for(int i=0; i< expected.length; i++){
             assertEquals(expected[i], result[i], 0.001f);
         }
     }
 
     @Test
-    public void testGetBufferedDataAndChangeNormals(){
+    public void testGetFace(){
         setUp();
-        var temp = new VertexInstance(v1, face, normal);
-        temp.setNormal(new NormalVector(new Vector3f(0.6f, 0.8f, 0f)));
-        float[] expected = {0f, 1f, 0f, 0.6f, 0.8f, 0f, -1f};
-        var result = temp.getBufferedData();
-        for(int i=0; i< expected.length; i++){
-            assertEquals(expected[i], result[i], 0.001f);
-        }
+
+        VertexInstance vertexInstance = new VertexInstance(v1, face);
+
+        Face expected = face;
+
+        Face result = vertexInstance.getFace();
+
+        assertThat(result).isEqualTo(expected);
     }
 }
